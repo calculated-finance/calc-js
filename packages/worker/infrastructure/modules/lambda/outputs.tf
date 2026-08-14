4c567acfc660fe009b4fee4ef82f769a3b4f5baf
Gad <gad@legion.cc>
2026-08-14 12:16:10 +1000

    feat(infra): manage CloudWatch dashboard

--- packages/worker/infrastructure/modules/lambda/outputs.tf
+++ packages/worker/infrastructure/modules/lambda/outputs.tf
@@ -1,3 +1,8 @@
+output "executor_function_names" {
+  description = "Names of the executor Lambda functions"
+  value       = aws_lambda_function.executor[*].function_name
+}
+
 output "prices_lambda_invoke_arn" {
   value = aws_lambda_function.prices.invoke_arn
 }
